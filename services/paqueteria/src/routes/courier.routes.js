4a0264aea592e8d26663cc6e60eb687640be1ff4
eduardorubio10 <eduardorubio@umes.edu.gt>
2026-04-16 17:24:38 -0600

    feat(paqueteria): sync latest upstream changes

--- services/paqueteria/src/routes/courier.routes.js
+++ services/paqueteria/src/routes/courier.routes.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const courierController = require('../controllers/courier.controller');
 
-router.get('/', courierController.getAll);
+router.get('/available', courierController.getAvailable);
 router.get('/:id', courierController.getById);
+router.get('/', courierController.getAll);
 router.post('/', courierController.create);
+router.put('/:id/status', courierController.updateCurrentStatus);
 router.put('/:id', courierController.update);
 router.delete('/:id', courierController.remove);
 
