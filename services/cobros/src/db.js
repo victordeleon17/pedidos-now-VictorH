e7b7fc0518ba7cfebc3b164db2539b23ea1e1e0c
Diego <diegojulian.bc@gmail.com>
2026-05-10 22:08:12 -0600

    feat: actualiza modulo de cobros con orm, migraciones y documentacion

--- services/cobros/src/db.js
+++ services/cobros/src/db.js
@@ -1,3 +1,17 @@
-const { sequelize } = require("../models");
+const mysql = require("mysql2/promise");
 
-module.exports = sequelize;
+const usaSsl = process.env.DB_SSL === "true";
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT || 3306),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  ssl: usaSsl ? { minVersion: "TLSv1.2" } : undefined
+});
+
+module.exports = pool;
